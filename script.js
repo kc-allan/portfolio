ee0af2aeb8a4bcbfd258ee49ba24a50d52cbb631
Allan <75929522+kc-allan@users.noreply.github.com>
2024-06-06 18:55:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const menu = document.querySelector('.ham-menu')
-const menuDropdown = document.querySelector('.menu-dropdown')
-menu.addEventListener('click', () => {
-	menu.classList.toggle('active')
-	menuDropdown.classList.toggle('active')
+document.addEventListener('DOMContentLoaded', () => {
+	const loading = document.querySelector('.loader')
+	loading.classList.add('hidden')
+	
+	const menu = document.querySelector('.ham-menu')
+	const menuDropdown = document.querySelector('.menu-dropdown')
+	menu.addEventListener('click', () => {
+		menu.classList.toggle('active')
+		menuDropdown.classList.toggle('active')
+	})
 })
